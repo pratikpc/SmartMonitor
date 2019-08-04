2e9d9ebec433884beef16711a967135ef533a5db
Pratik Chowdhury <pratikc@live.co.uk>
2019-08-04 17:45:34 +0530

    login page and file upload page working.
     Submit button restriction pending
    updated

--- Server/Website/js/rasplistgen.js
+++ Server/Website/js/rasplistgen.js
@@ -4,20 +4,27 @@ $(document).ready(() => {
     });
 });
 
+var i = 0;
 function addCheckbox(displays) {
     const dispId = document.getElementById("Displays");
     displays.forEach(display => {
+
         const checkbox = document.createElement("input");
         checkbox.setAttribute("type", "checkbox");
         checkbox.setAttribute("name", "ids");
         checkbox.setAttribute("value", display.id);
+        checkbox.setAttribute("id", "checkBox" + i);
+        checkbox.setAttribute("class", "allboxes");
 
         const label = document.createElement("label");
         label.innerText = display.name;
+        label.setAttribute("for", "checkBox" + i);
 
         const div = document.createElement("div");
         div.appendChild(checkbox);
         div.appendChild(label);
         dispId.appendChild(div);
+
+        i = i + 1;
     });
 }
