93eba15f355f4d65563cd880578dec7a09f123bc
Pratik Chowdhury <pratikc@live.co.uk>
2019-11-30 22:47:55 +0530

    Made startup completely synchronous

--- Server/lib/app.ts
+++ Server/lib/app.ts
@@ -2,7 +2,7 @@ import * as dotenv from "dotenv"
 
 // If No import taking place from .env file
 // Use this to ensure that dotenv file is loaded
-if (!process.env.Dialect){
+if (!process.env.Dialect) {
   dotenv.config();
 }
 
@@ -14,52 +14,58 @@ import * as cors from "cors";
 import { PassportModelsGenerate } from "./Models/Passport.Models";
 import { RoutesCommon } from "./routes/Common.Routes";
 
-export const app = express();
-// app.use(bodyParser.json());
-// middleware for parsing application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+export async function App() {
 
-// Use this to Render HTML
+  const app = express();
 
-// Specify Path of Website Static Contents
-app.engine('.html', require('ejs').renderFile);
-app.set("views", "./Views");
-app.set('view engine', 'ejs');
+  // app.use(bodyParser.json());
+  // middleware for parsing application/x-www-form-urlencoded
+  app.use(bodyParser.urlencoded({ extended: true }));
 
-// Only Enable if our app is not dockerised
-// In Dockerised App, static files are served by
-// NGINX
-// Thus reducing the load on server
-if(!process.env.APP_IS_DOCKERISED){
-  app.use("/static/", express.static("./Website"));
+  // Use this to Render HTML
+
+  // Specify Path of Website Static Contents
+  app.engine('.html', require('ejs').renderFile);
+  app.set("views", "./Views");
+  app.set('view engine', 'ejs');
+
+  // Only Enable if our app is not dockerised
+  // In Dockerised App, static files are served by
+  // NGINX
+  // Thus reducing the load on server
+  if (!process.env.APP_IS_DOCKERISED) {
+    app.use("/static/", express.static("./Website"));
+  }
+
+  await Models.RunSynchronisation();
+  PassportModelsGenerate(app);
+
+  app.use(cors.default());
+  // middleware for json body parsing
+  app.use(bodyParser.json());
+  // middleware for parsing application/x-www-form-urlencoded
+  app.use(bodyParser.urlencoded({ extended: true }));
+  // middleware for json body parsing
+  app.use(bodyParser.json({ limit: "20mb" }));
+
+  // Route via this as Path to Users
+  app.use("/user", Routes.Users);
+  // Route via this as Path for Display
+  app.use("/display", Routes.Displays);
+  // Route via this as path for File Uploading and Downloading
+  app.use("/files", Routes.Files);
+
+  app.get("/navbar", RoutesCommon.IsAuthenticated, (req, res) => {
+    if (RoutesCommon.GetUser(req).Authority === "ADMIN")
+      return res.render("navbaradmin.html");
+    else
+      return res.render("navbarnormal.html");
+  });
+
+  app.get("/", (req, res) => {
+    if (req.isUnauthenticated()) return res.render("login.html");
+    return res.redirect("/files/upload");
+  });
+
+  return app;
 }
-
-Models.RunSynchronisation().then(() => { });
-PassportModelsGenerate(app);
-
-app.use(cors.default());
-// middleware for json body parsing
-app.use(bodyParser.json());
-// middleware for parsing application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
-// middleware for json body parsing
-app.use(bodyParser.json({ limit: "20mb" }));
-
-// Route via this as Path to Users
-app.use("/user", Routes.Users);
-// Route via this as Path for Display
-app.use("/display", Routes.Displays);
-// Route via this as path for File Uploading and Downloading
-app.use("/files", Routes.Files);
-
-app.get("/navbar", RoutesCommon.IsAuthenticated, (req, res) => {
-  if (req.user.Authority === "ADMIN")
-    return res.render("navbaradmin.html");
-  else
-    return res.render("navbarnormal.html");
-});
-
-app.get("/", (req, res) => {
-  if (req.isUnauthenticated()) return res.render("login.html");
-  return res.redirect("/files/upload");
-});
--- Server/lib/server.ts
+++ Server/lib/server.ts
@@ -1,11 +1,13 @@
 import * as config from './config/server'
-import {app} from "./app";
+import { App } from "./app";
 
-app.listen(config.Server.Port, config.Server.Name, () => {
-  console.log("Default Login Screen", config.Server.Name + ':' + config.Server.Port);
-});
+App().then((app) => {
+  app.listen(config.Server.Port, config.Server.Name, () => {
+    console.log("Default Login Screen", config.Server.Name + ':' + config.Server.Port);
+  });
 
-process.on("SIGINT", function() {
-  console.log("App Shutting Down");
+  process.on("SIGINT", function () {
+    console.log("App Shutting Down");
+  });
 });
 
