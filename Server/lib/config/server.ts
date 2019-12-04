b7baf52d6da138abf0e306a5731b7d39054b3181
Pratik Chowdhury <pratikc@live.co.uk>
2019-12-05 03:16:40 +0530

    Media Storage Path is now part of Configuration

--- Server/lib/config/server.ts
+++ Server/lib/config/server.ts
@@ -1,6 +1,8 @@
 import {GetIP} from "./Config.Common";
+import * as Path from "path";
 
 export const Server = {
   Port: 8000,
-  Name: GetIP()
+  Name: GetIP(),
+  MediaStorage: Path.resolve(String(process.env.INIT_CWD), 'uploads')
 };
