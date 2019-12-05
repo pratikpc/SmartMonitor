7f9214bd45019e0fd38870fdef3b598507c7eb73
Pratik Chowdhury <pratikc@live.co.uk>
2019-12-05 06:35:24 +0530

    Display delete is Delete Request at /display/:id

--- Server/lib/routes/Display.Routes.ts
+++ Server/lib/routes/Display.Routes.ts
@@ -196,7 +196,7 @@ Displays.get("/:id/files", RoutesCommon.IsAdmin, async (req, res) => {
   });
 });
 
-Displays.delete("/", RoutesCommon.IsAdmin, async (req, res) => {
+Displays.delete("/:id", RoutesCommon.IsAdmin, async (req, res) => {
   try {
     const params = RoutesCommon.GetParameters(req);
     if (params == null)
