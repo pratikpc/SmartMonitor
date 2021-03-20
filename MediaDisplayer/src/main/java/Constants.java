4eaed428c902da4e92731d884a3ddad3dece9605
Pratik Chowdhury <pratikc@live.co.uk>
2021-03-21 05:04:16 +0530

    Remove JavaFX code from WebUI

--- MediaDisplayer/src/main/java/Constants.java
+++
@@ -1,8 +0,0 @@
-public class Constants {
-    public static final String AppName = "Media Displayer";
-    public static final int Port = 8000;
-
-    static class DB {
-        public static final String FileT = "FileT";
-    }
-}
